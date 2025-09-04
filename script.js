c1691da6ec01b66f850a9780f56f3416b82a11c7
Johnny Noh <ima.john.noh@gmail.com>
2025-09-05 05:03:24 +0900

    Fix parserExpression's bug

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ const parseExpressions = (expr) => {
   expr = expr.replace(/−/g, "-");
   const tokens = expr.match(/-?\d+(?:\.\d+)?|[÷×+-]/g);
 
-  if (/[÷×+-]/.test(tokens.slice(-1))) tokens.pop();
+  if (/[÷×+-]$/.test(tokens.slice(-1))) tokens.pop();
 
   return tokens;
 };
